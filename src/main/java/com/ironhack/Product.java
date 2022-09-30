8a6e617362ad1fdb2fbf62bfb1261ba05851113d
mirnaia1983 <ariira1983@gmail.com>
2022-09-30 22:58:55 +0200

    Blueprint

---
+++ src/main/java/com/ironhack/Product.java
@@ -0,0 +1,7 @@
+package com.ironhack;
+
+public enum Product {
+    HYBRID,
+    FLATBED,
+    BOX
+}
---
+++ src/main/java/com/ironhack/Status.java
@@ -0,0 +1,7 @@
+package com.ironhack;
+
+public enum Status {
+    OPEN,
+    CLOSED_WON,
+    CLOSED_LOST
+}

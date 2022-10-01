aa927169e67064131acf17f04cc726a688438cea
Quim Crous <joaquimcrous@gmail.com>
2022-10-01 08:48:01 +0200

    ID override contact subclass

--- src/main/java/com/ironhack/classes/Contact.java
+++ src/main/java/com/ironhack/classes/Contact.java
@@ -1,9 +1,15 @@
 package com.ironhack.classes;
 
 public class Contact extends Lead{
+    private static int counter2 = 0;
 
     public Contact(String name, Long phoneNumber, String email, String companyName) {
         super(name, phoneNumber, email, email);
+    }
+
+    @Override
+    public void setID(){
+        this.id = counter2++;
     }
 
 }

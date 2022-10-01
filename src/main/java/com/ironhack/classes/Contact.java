2490d011092f678ce32204583ee025f0264e2a06
Anya <anya.chocolat@gmail.com>
2022-10-01 16:35:11 +0200

    update

--- src/main/java/com/ironhack/classes/Contact.java
+++ src/main/java/com/ironhack/classes/Contact.java
@@ -1,16 +1,55 @@
 package com.ironhack.classes;
 
-public class Contact extends Lead{
+public class Contact{
     private static int counter2 = 0;
     private final int id2;
+    private String name;
+    private long phoneNumber;
+    private String email;
+    private String companyName;
 
-    public Contact(String name, Long phoneNumber, String email, String companyName) {
-        super(name, phoneNumber, email, companyName);
+    public Contact(String name, long phoneNumber, String email, String companyName) {
         this.id2 = counter2++;
+        setName(name);
+        setPhoneNumber(phoneNumber);
+        setEmail(email);
+        setCompanyName(companyName);
     }
 
     public int getId2() {
         return id2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
     }
 
     @Override

e39a57c207f17b89a74ad6f20f7fae6ba6d6345a
Quim Crous <joaquimcrous@gmail.com>
2022-10-01 10:05:22 +0200

    Fixed

--- src/main/java/com/ironhack/classes/Contact.java
+++ src/main/java/com/ironhack/classes/Contact.java
@@ -4,12 +4,10 @@ public class Contact extends Lead{
     private static int counter2 = 0;
 
     public Contact(String name, Long phoneNumber, String email, String companyName) {
+        this.id = counter2;
         super(name, phoneNumber, email, email);
+        counter2 ++;
     }
 
-    @Override
-    public void setId(){
-        this.id = counter2++;
-    }
 
 }

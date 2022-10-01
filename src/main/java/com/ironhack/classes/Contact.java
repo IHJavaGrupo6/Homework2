7d00f39c4c56f6188a685ceedd9bec8e21ed2e73
Quim Crous <joaquimcrous@gmail.com>
2022-10-01 07:36:18 +0200

    Enums added + Contact Class created

---
+++ src/main/java/com/ironhack/classes/Contact.java
@@ -0,0 +1,9 @@
+package com.ironhack.classes;
+
+public class Contact extends Lead{
+
+    public Contact(String name, Long phoneNumber, String email, String companyName) {
+        super(name, phoneNumber, email, email);
+    }
+
+}
---
+++ src/main/java/com/ironhack/enums/Status.java
@@ -0,0 +1,5 @@
+package com.ironhack.enums;
+
+public enum Status {
+    OPEN, CLOSED_WON, CLOSED_LOST
+}
